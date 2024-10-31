21213039d3f0cda8c5841ff8f0ac40ee7846dec6
gabrielbwitte <119006614+gabrielbwitte@users.noreply.github.com>
2024-10-31 10:56:30 -0400

    avanço da aula

--- react cadastro/src/pages/cadastro/index.jsx
+++ react cadastro/src/pages/cadastro/index.jsx
@@ -1,16 +1,37 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function Cadastro() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await api.post('/cadastro', {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+      alert('usuario cadastrado')
+    } catch (error) {
+      alert("erro ao cadastrar o usuario.")
+    }
+  }
+
   return (
-    <div className="bg-black">
-      <h2>Casdratro</h2>
-      <form>
-        <input placeholder="Nome" type="text" />
-        <input placeholder="Email" type="email" />
-        <input placeholder="Senha" type="password" />
-        <button>Cadastrar-se</button>
+    <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Casdratro</h2>
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <input ref={nameRef} placeholder="Nome" type="text" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={emailRef} placeholder="Email" type="email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={passwordRef} placeholder="Senha" type="password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400">Cadastrar-se</button>
       </form>
-      <Link to={"/login"}>Já tem uma conta? Faça o login</Link>
+      <Link to={"/login"} className="text-blue-700 hover:underline mt-4 block text-center">Já tem uma conta? Faça o login</Link>
     </div>
   );
 }
--- react cadastro/src/pages/login/index.jsx
+++ react cadastro/src/pages/login/index.jsx
@@ -1,7 +1,35 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+
 function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const { data: token } = await api.post('/login', {
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+      console.log(token)
+      alert('Login feito')
+    } catch (error) {
+      alert("Senha incoreta.")
+    }
+  }
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Login</h2>
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <input ref={emailRef} placeholder="Email" type="email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={passwordRef} placeholder="Senha" type="password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400">Login</button>
+      </form>
+      <Link to={"/"} className="text-blue-700 hover:underline mt-4 block text-center">Não tem uma conta? Cadastra-se</Link>
     </div>
   );
 }
